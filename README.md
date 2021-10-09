41099f83760aa5554256285ad07626ca66d96980
Zachary Hafen <zachary.h.hafen@gmail.com>
2021-10-09 10:36:05 -0700

    Added basic functionality.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # transcribe-spanish
 Uses IBM Watson to perform Spanish transcription of .m4a files.
+
+# Requirements
+ffmpeg installed
+Recommended: install brew, then install ffmpeg.
+
+git installed
+python installed, verdict module installed
---
+++ zach.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+### Customize these two lines!!
+export APIKEY=YOURAPIKEYHERE
+export URL=YOURURLHERE
+
+# Output files
+WAVFILE=${1/m4a/wav}
+TRANSCRIPT=${1/m4a/json}
+TRANSCRIPT=${TRANSCRIPT/audio/transcripts}
+
+# Convert to wav
+ffmpeg -i $1 $WAVFILE
+
+# Perform transcription
+curl -X POST -u apikey:$APIKEY --header "Content-Type: audio/wav" --data-binary @$WAVFILE "$URL/v1/recognize?model=es-MX_BroadbandModel" > $TRANSCRIPT
